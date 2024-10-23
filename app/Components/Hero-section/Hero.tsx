f69ff269b6ffae7faa19d83a6a86059a245d4414
MHassaanQureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2024-10-23 19:14:14 +0500

    Add files via upload

--- app/Components/Hero-section/Hero.tsx
+++ app/Components/Hero-section/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero(){
             <div className="flex flex-col align-middle">
                 <h1 className="text-4xl font-extrabold w-[27rem] pb-4">Hi, I am John, Creative Technologist</h1>
                 <p className="flex w-3/5 pb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor magnam voluptate eaque quia in reprehenderit, voluptatibus maxime repellendus. Deserunt blanditiis iure numquam amet, accusantium suscipit? Quos obcaecati laudantium saepe quasi laborum nam esse.</p>
-                <button className="flex justify-center items-center w-52 bg-[#F98585] p-2 text-white">Download Resume</button>
+                <button className="flex justify-center items-center w-52 bg-[#F98585] p-2 text-white shadow-xl">Download Resume</button>
 
 
             </div>
